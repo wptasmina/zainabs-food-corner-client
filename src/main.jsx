a84d6f4f91219f87a6171ba8ff66d01fd95eb0d5
wptasmina <wptasmina@gmail.com>
2024-12-31 00:40:29 +0600

    Router, Layout,Pages and component folder added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { router } from './Routes/Routes';
+// import App from './App.jsx'
+
+import {
+  RouterProvider,
+} from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
